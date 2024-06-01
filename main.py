d1ba67afda9ee69894ffad89cbc762cc2f57673b
LeznyovArtyom <127618249+LeznyovArtyom@users.noreply.github.com>
2024-06-01 15:42:53 +0700

    Изменил переподключение к бд для развертки

--- main.py
+++ main.py
@@ -597,5 +597,5 @@ def update_review(id: int, review_data: ReviewUpdate):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    # uvicorn.run(app, host="0.0.0.0", port=os.getenv("MYSQLPORT"))
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=os.getenv("MYSQLPORT"))
